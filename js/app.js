1b67b252e9a7fe1472dc95fc0c7ebb26c28275b2
mamun8893 <0316361936@student.wub.edu.bd>
2021-08-18 20:58:12 +0600

    create calculator and clear value

--- js/app.js
+++ js/app.js
@@ -17,3 +17,20 @@ document.getElementById('pin-generate').addEventListener('click', function() {
     const pin = generatePin();
     document.getElementById('pin-input').value = pin
 })
+
+//Number type
+
+document.getElementById('key-pad').addEventListener('click', function(event) {
+    const number = event.target.innerText;
+    const calcInput = document.getElementById('typed-number');
+    if (isNaN(number)) {
+        if (number == 'C') {
+            calcInput.value = ''
+        }
+    } else {
+        const prevoisNumber = calcInput.value;
+        const newNUmber = prevoisNumber + number;
+        calcInput.value = newNUmber;
+    }
+
+})
